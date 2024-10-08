ac89fa77fea79c66ca5316580db07c7b29ef911c
Roy Bao <roy@roybao.com>
2024-10-08 16:23:48 -0700

    Add xcuid as dependency and use it for BillionOak.schema

---
+++ lib/billion_oak/schema.ex
@@ -0,0 +1,23 @@
+defmodule BillionOak.Schema do
+  defmacro __using__(opts) do
+    id_prefix = Keyword.fetch!(opts, :id_prefix)
+
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+
+      @type t :: %__MODULE__{}
+
+      @primary_key {:id, :string, autogenerate: false}
+      @foreign_key_type :string
+      @timestamps_opts [type: :utc_datetime_usec]
+
+      def generate_id, do: "#{unquote(id_prefix)}_" <> XCUID.generate()
+      def id_prefix, do: unquote(id_prefix)
+      def prefix_id(id), do: "#{unquote(id_prefix)}_" <> id
+
+      def changeset(%{id: nil} = struct), do: change(struct, id: generate_id())
+      def changeset(struct), do: change(struct)
+    end
+  end
+end
