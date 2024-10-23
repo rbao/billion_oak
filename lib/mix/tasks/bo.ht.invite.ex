f633750a1f69c7ac51eb27f0e8e8318d6acb9c1c
Roy Bao <roy@roybao.com>
2024-10-22 21:45:59 -0700

    Add mix task

---
+++ lib/mix/tasks/bo.ht.invite.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Bo.Ht.Invite do
+  use Mix.Task
+  alias BillionOak.Request
+
+  @shortdoc "Create a invitation code for happy team associate"
+
+  @moduledoc """
+  This is where we would put any long form documentation and doctests.
+  """
+  @requirements ["app.start"]
+  @impl Mix.Task
+  def run([company_account_rid]) do
+    req = %Request{_role_: "sysops", identifier: %{handle: "happyteam"}}
+    {:ok, %{data: organization}} = BillionOak.get_organization(req)
+
+    req = %Request{
+      _role_: "sysops",
+      data: %{organization_id: organization.id, invitee_company_account_rid: company_account_rid}
+    }
+
+    result = BillionOak.create_invitation_code(req)
+    case result do
+      {:ok, %{data: invitation_code}} -> IO.inspect(invitation_code.value)
+      {:error, changeset} -> IO.inspect(changeset.errors)
+    end
+  end
+end
