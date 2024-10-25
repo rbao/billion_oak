eb59255cf7822f703bb5a766cf38eaf3f9a9267b
Roy Bao <roy@roybao.com>
2024-10-24 18:59:10 -0700

    Clean up test

--- test/billion_oak_web/jwt_test.exs
+++ test/billion_oak_web/jwt_test.exs
@@ -2,10 +2,8 @@ defmodule BillionOakWeb.JWTTest do
   use BillionOak.DataCase
   alias BillionOakWeb.JWT
 
-  describe "clients" do
-    test "it works" do
-      {:ok, token, _claims} = JWT.generate_and_sign(%{aud: "test"})
-      assert is_binary(token)
-    end
+  test "JWT token can be generated" do
+    {:ok, token, _claims} = JWT.generate_and_sign(%{aud: "test"})
+    assert is_binary(token)
   end
 end
